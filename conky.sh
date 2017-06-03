cc41a0457638a62bb686923527b42b7039097c3f
Infinite <Infinitetutts@users.noreply.github.com>
2017-06-03 16:07:17 +0200

    Create conky.sh

---
+++ conky.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# A bash script that starts two separate conky configurations
+#
+# this script starts the configs meant for a single-screened system
+#   this means that the trees are placed on each side of the screen opposite of
+#   each other
+
+# starts conky scripts
+sleep 5 # Wait 5 seconds for boot
+conky -dc ./conky_left.conf
+conky -dc ./conky_right.conf
